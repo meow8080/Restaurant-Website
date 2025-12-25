82bfd1e59de5c06fdb5a09bc7611e1b14895739f
meow8080 <komu5530@gmail.com>
2025-12-25 11:42:07 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="bg-black  cursor-pointer font-bold text-white shadow-md px-6 py-4 flex justify-between items-center">
+    <nav className="bg-black max-w-auto cursor-pointer font-bold text-white shadow-md px-6 py-4 flex flex-row justify-between items-center">
       <h1 className="text-3xl">SeaSide Heaven</h1>
       <div className="flex justify-end gap-8">
         <Link to="/home" className="hover:text-violet-400">Home</Link>
