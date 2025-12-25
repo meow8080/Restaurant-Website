1abca91c1f594cf261cfcd699e412db30ac9e2d4
meow8080 <komu5530@gmail.com>
2025-12-25 11:52:56 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="bg-black max-w-auto cursor-pointer font-bold text-white shadow-md px-6 py-4 flex flex-row justify-between items-center">
       <h1 className="text-3xl">SeaSide Heaven</h1>
-      <div className="flex justify-end gap-8">
+      <div className="flex flex-col sm:flex-row justify-end gap-8">
         <Link to="/home" className="hover:text-violet-400">Home</Link>
         <Link to="/menu" className="hover:text-violet-400">Menu</Link>
         <Link to="/about" className="hover:text-violet-400">About</Link>
